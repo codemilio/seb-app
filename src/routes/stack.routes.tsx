7749746f44617e9f74834d2e0e0dbf836a561342
DanPendragom <carlosemilio.contato@gmail.com>
2023-11-28 09:12:51 -0300

    fix: navigation
    Adding new routes: Recover Password, Recover Success, Register for Client and Seller

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,14 +1,26 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import { Login, Welcome } from '../screens'
+import { 
+    Login, 
+    Welcome, 
+    Register, 
+    RegisterForm, 
+    RecoverPassword, 
+    RecoverPasswordSuccess
+} from '../screens'
+import { RootStackPropList } from '../utils/hooks/navigation'
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator<RootStackPropList>()
 
 export default function StackRoutes() {
     return(
         <Stack.Navigator initialRouteName="welcome" screenOptions={{ headerShown: false }}>
             <Stack.Screen name="welcome" component={Welcome}/>
             <Stack.Screen name="login" component={Login}/>
+            <Stack.Screen name="register" component={Register}/>
+            <Stack.Screen name="registerForm" component={RegisterForm}/>
+            <Stack.Screen name="recoverPassword" component={RecoverPassword}/>
+            <Stack.Screen name="recoverPasswordSuccess" component={RecoverPasswordSuccess}/>
         </Stack.Navigator>
     )
 }
--- src/utils/hooks/navigation.ts
+++ src/utils/hooks/navigation.ts
@@ -4,6 +4,12 @@ import type { NativeStackNavigationProp } from "@react-navigation/native-stack"
 export type RootStackPropList = {
     welcome: undefined
     login: undefined
+    register: undefined
+    recoverPassword: undefined
+    recoverPasswordSuccess: undefined
+    registerForm: {
+        role: "CLIENT" | "SELLER"
+    }
 }
 
 export type NavigationProp = NativeStackNavigationProp<RootStackPropList>
