9568482a2a4f9967beaac9784a0bca1230bfd66e
DanPendragom <carlosemilio.contato@gmail.com>
2023-12-01 16:00:37 -0300

    fix: searchBar

--- src/components/HeaderTab/index.tsx
+++ src/components/HeaderTab/index.tsx
@@ -1,12 +1,14 @@
+import { ViewProps } from "react-native"
 import { Text, View } from "react-native"
+import { twMerge } from "tailwind-merge"
 
-type Props = {
+type Props = ViewProps & {
     label: string
 }
 
-export default function HeaderTab({ label }: Props){
+export default function HeaderTab({ label, ...props }: Props){
     return(
-        <View className="mx-auto border-b-2 border-[#E1E1E1] w-2/3 pb-2 mb-8">
+        <View className={twMerge("mx-auto border-b-2 border-[#E1E1E1] w-2/3 pb-2 mb-8", props.className)}>
             <Text className="uppercase text-2xl text-center font-semibold">{label}</Text>
         </View>
     )
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,25 +1,54 @@
-import { View, Image, TextInput } from "react-native";
+import { View, Image, TextInput, TouchableOpacity, ViewProps } from "react-native";
+import { DrawerActions } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/FontAwesome5";
+import classNames from "classnames";
+
 import HeaderLogo from '../../assets/headerLogo.png';
+import useTypedNavigation from "../../utils/hooks/navigation";
+import Separator from "../Separator";
 
-export default function SearchBar({  }) {
+type Props = ViewProps & {
+    showTextInput?: boolean
+}
+
+export default function SearchBar({ showTextInput = true, ...props }: Props) {
+    const navigation = useTypedNavigation()
+
     return(
-        <View>
+        <View className={classNames("flex flex-col items-center px-5 pb-6 space-y-4", {
+            "flex-row-reverse justify-between": !showTextInput
+        })}>
+            { !showTextInput && <Separator className="w-0  h-0" /> }
+            <Image 
+                className={classNames("", {
+                    "-ml-8": !showTextInput
+                })}
+                source={HeaderLogo} 
+                resizeMode="contain" 
+                resizeMethod="scale" 
+                style={{ height: 80 }} 
+            />
+            <View className="flex flex-row space-x-2">
+                <TouchableOpacity
+                    onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+                    className={classNames("flex flex-shrink-0 w-8 justify-center items-center")}
+                >
+                    <Icon name="bars" size={24} color="#555"/>
+                </TouchableOpacity>
 
-            <View className="flex-1 flex-col items-center p-5 space-y-4">
-                <Image source={HeaderLogo} resizeMode="contain" resizeMethod="scale" style={{ height: 80 }} />
-                <View className="flex flex-row w-full h-10 rounded-md items-center px-4 py-2 bg-[#D9D9D9] space-x-4">
-                    <Icon className="flex-shrink-0" name="search" color="#555" size={16}/>
-                    <TextInput className="flex-1" placeholder="O que você busca?"/>
-                    <Icon className="flex-shrink-0" name="microphone" color="#555" size={16}/>
-                </View>
+                {showTextInput && (
+                    <View className="flex-1 flex-row w-full h-10 rounded-md items-center px-4 py-2 bg-[#D9D9D9] space-x-4">
+                        <Icon className="flex-shrink-0" name="search" color="#555" size={16}/>
+                        <TextInput className="flex-1" placeholder="O que você busca?"/>
+                        <TouchableOpacity
+                            className="flex-shrink-0"
+                        >
+                            <Icon name="microphone" color="#555" size={16}/>
+                        </TouchableOpacity>
+                    </View>
+                )}
             </View>
         </View>
     )   
 }
 
-// <TouchableOpacity
-//         onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}>
-//         {/* Ícone do menu ou texto do botão, conforme sua preferência */}
-//         <Text style={styles.menuButtonText}>Menu</Text>
-//       </TouchableOpacity>
